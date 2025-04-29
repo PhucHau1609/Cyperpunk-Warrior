f1a022a7499ed849f92ab45354366ebdf3312f88
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2025-04-29 16:52:06 +0700

    Basic Movement,Jump,Death Animation
    Jump animation not done

---
+++ Assets/Hau/_Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 12f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float rayLength = 0.5f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private float moveInput;
+    private bool isGrounded;
+    private bool isJumping;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleAnimation();
+        FlipSprite();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        CheckGround();
+    }
+
+    void HandleInput()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = false;
+        }
+    }
+
+    void CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, rayLength, groundLayer);
+        isGrounded = hit.collider != null;
+
+        Debug.DrawRay(groundCheck.position, Vector2.down * rayLength, isGrounded ? Color.green : Color.red);
+    }
+
+
+    void FlipSprite()
+    {
+        if (moveInput > 0)
+            spriteRenderer.flipX = false;
+        else if (moveInput < 0)
+            spriteRenderer.flipX = true;
+
+        if (!Input.GetMouseButtonDown(2)) return;
+        animator.SetTrigger("Death");
+        Destroy(gameObject,1f);
+
+    }
+
+    void HandleAnimation()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("VerticalVelocity", rb.velocity.y);
+    }
+}
