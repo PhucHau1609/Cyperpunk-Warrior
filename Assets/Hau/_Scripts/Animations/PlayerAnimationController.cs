57d5689be54cfd7d2179acc0357640892efd9253
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2025-05-03 22:47:10 +0700

    70percent_feature/008

---
+++ Assets/Hau/_Scripts/Animations/PlayerAnimationController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerAnimationState currentState;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ChangeState(PlayerAnimationState newState)
+    {
+        if (newState == currentState) return;
+
+        currentState = newState;
+
+        // Option 1: Trigger-based animation
+        animator.Play(newState.ToString());
+
+        // Option 2: Nếu dùng bool/trigger:
+        ResetAllBools();
+        animator.SetBool(newState.ToString(), true);
+    }
+
+    private void ResetAllBools()
+    {
+        foreach (PlayerAnimationState state in System.Enum.GetValues(typeof(PlayerAnimationState)))
+        {
+            animator.SetBool(state.ToString(), false);
+        }
+    }
+
+    public void SetFloat(string name, float value) => animator.SetFloat(name, value);
+    public void SetBool(string name, bool value) => animator.SetBool(name, value);
+    public void Trigger(string name) => animator.SetTrigger(name);
+
+}
---
+++ Assets/Hau/_Scripts/Animations/PlayerAnimationState.cs
@@ -0,0 +1,16 @@
+﻿public enum PlayerAnimationState
+{
+    Idle,
+    Run,
+    Jump,
+    Fall,
+    Attack1,
+    Attack2,
+    Attack3,
+    Attack4,
+    Crouch,
+    Dash,
+    Hurt,
+    Die
+    // Thêm tùy ý
+}
