8871c366cdfb79be17b53ee94a99f56d963b0530
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2025-05-10 12:27:00 +0700

    melee attack
    not done

--- Assets/_Hau/_Scripts/Inventory/Items/ItemCode.cs
+++ Assets/_Hau/_Scripts/Inventory/Items/ItemCode.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public enum ItemCode
 {
+    //Items,Gold,Currency
     NoName = 0,
     Gold = 1,
     EMP = 2,
+    //Melee Weapons
+    Knife_1 = 50,
 
-
+    //Guns Weapons
     Gun_1 = 100,
     Gun_2 = 101,
 
+    //Health
     PotionMana = 200,
 }
