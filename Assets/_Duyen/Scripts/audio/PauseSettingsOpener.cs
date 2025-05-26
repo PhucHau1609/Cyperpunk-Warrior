cf26886ceacf490cd1cd6468480e5fc054f99d99
beduyencute <ngothuymyduyen1411@gmail.com>
2025-05-26 22:50:34 +0700

    feature/Sunny-0047-5

---
+++ Assets/_Duyen/Scripts/audio/PauseSettingsOpener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseSettingsOpener : MonoBehaviour
+{
+    [SerializeField] private Button settingsButton;
+
+    void Start()
+    {
+        settingsButton.onClick.AddListener(() =>
+        {
+            AudioManager.Instance.PlayClickSFX();
+            AudioSettingsUI.Instance.OpenSettingsPanel();
+        });
+    }
+}
