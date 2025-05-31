3639da9bc32421903dc8b303cd21e44c075f416a
Minh Thuan <141199630+Pee2306@users.noreply.github.com>
2025-05-31 22:35:29 +0700

    feature/Sunny-0055 V2

--- Assets/_Thuan/Scripts/DialogueSystemCutscene/DialogueTrigger.cs
+++ Assets/_Thuan/Scripts/DialogueSystemCutscene/DialogueTrigger.cs
@@ -3,15 +3,36 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public GameObject speechIcon;
-    public GameObject dialogueCanvas;
+    public GameObject dialogueCanvas; 
+    public Vector3 iconOffset = new Vector3(0, 1.5f, 0); 
 
     private bool playerInRange = false;
     private bool hasTalked = false;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        if (speechIcon != null)
+            speechIcon.SetActive(false);
+
+        if (dialogueCanvas != null)
+            dialogueCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Di chuyển icon theo đầu Player
+        if (playerInRange && playerTransform != null && speechIcon != null)
+        {
+            speechIcon.transform.position = playerTransform.position + iconOffset;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !hasTalked)
         {
+            playerTransform = collision.transform;
             speechIcon.SetActive(true);
             playerInRange = true;
         }
@@ -23,16 +44,19 @@ public class DialogueTrigger : MonoBehaviour
         {
             speechIcon.SetActive(false);
             playerInRange = false;
+            playerTransform = null;
         }
     }
 
     public void OnSpeechIconClicked()
     {
-        if (!hasTalked && playerInRange)
+        if (playerInRange && !hasTalked)
         {
-            dialogueCanvas.SetActive(true);
-            speechIcon.SetActive(false);  // Ẩn biểu tượng sau khi bấm
-            hasTalked = true;             // Đánh dấu đã nói chuyện
+            if (dialogueCanvas != null)
+                dialogueCanvas.SetActive(true);
+
+            speechIcon.SetActive(false);
+            hasTalked = true;
         }
     }
 }
