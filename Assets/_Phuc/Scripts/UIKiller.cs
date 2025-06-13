2a67bc13153fabd8639f9f4d6fca76acdbe7bfea
trongphuc <phucnhtps39905@gmail.com>
2025-06-13 12:48:19 +0700

    fix Loading

---
+++ Assets/_Phuc/Scripts/UIKiller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UIKiller : MonoBehaviour
+{
+    void Awake()
+    {
+        string[] uiNamesToKill = { "IU_GAME", "CanvasGameplay", "HUD", "PlayerUI" }; // Tên UI bạn muốn xóa
+
+        foreach (var go in FindObjectsOfType<GameObject>())
+        {
+            foreach (var name in uiNamesToKill)
+            {
+                if (go.name.Contains(name))
+                {
+                    Destroy(go);
+                    Debug.Log($"[UIKiller] Destroyed leftover UI: {go.name}");
+                }
+            }
+        }
+    }
+}
