90b4bbe278bb858538b7d22ddba56b65bb181407
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2025-06-22 22:25:08 +0700

    optimizer some code, add shield ability , add windzone

---
+++ Assets/_Hau/_Scripts/Ability/PlayerShield.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerShield : PlayerAbility
+{
+    [Header("Shield Settings")]
+    [SerializeField] private GameObject shieldVisual; 
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TryUseAbility();
+        }
+    }
+
+    protected override void OnAbilityStart()
+    {
+        if (shieldVisual != null)
+            shieldVisual.SetActive(true);
+
+        Debug.Log("Shield activated!");
+    }
+
+    protected override void OnAbilityEnd()
+    {
+        if (shieldVisual != null)
+            shieldVisual.SetActive(false);
+
+        Debug.Log("Shield deactivated!");
+    }
+}
