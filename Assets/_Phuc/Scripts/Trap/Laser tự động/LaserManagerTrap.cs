cf5907d4cc8e5f25dfa64c1ab6853205916cc9c4
trongphuc <phucnhtps39905@gmail.com>
2025-06-23 18:30:18 +0700

    feature/Sunny-0093-0094

---
+++ Assets/_Phuc/Scripts/Trap/Laser tự động/LaserManagerTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserManagerTrap  : MonoBehaviour
+{
+    public List<GameObject> lasers;       // Danh sách laser đã tắt sẵn
+    public float delayBetweenLasers = 0.2f; // Khoảng cách thời gian giữa từng laser
+
+    public void ActivateLasers()
+    {
+        StartCoroutine(ActivateLasersInOrder());
+    }
+
+    private IEnumerator ActivateLasersInOrder()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(true); // Mở laser lên
+            yield return new WaitForSeconds(delayBetweenLasers);
+        }
+    }
+}
