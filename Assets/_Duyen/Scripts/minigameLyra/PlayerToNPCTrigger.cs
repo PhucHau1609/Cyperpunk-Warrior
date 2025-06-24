9ce8127e0db68a841ed03dc07250d80524fb510e
beduyencute <ngothuymyduyen1411@gmail.com>
2025-06-24 14:45:15 +0700

    feature/Sunny-0080-18

---
+++ Assets/_Duyen/Scripts/minigameLyra/PlayerToNPCTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerToNPCTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneController controller = FindFirstObjectByType<SceneController>();
+            if (controller != null)
+            {
+                controller.SwitchToPetControl();
+                gameObject.SetActive(false); // tắt trigger nếu chỉ dùng 1 lần
+            }
+        }
+    }
+}
