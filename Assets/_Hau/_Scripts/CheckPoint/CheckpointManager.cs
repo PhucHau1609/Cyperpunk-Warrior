7c7b56e9a8366bc19a801484493dacfbd53bfa46
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2025-06-30 10:06:37 +0700

    check point v1

---
+++ Assets/_Hau/_Scripts/CheckPoint/CheckpointManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckpointManager : HauSingleton<CheckpointManager>
+{
+    public CheckPointEnum lastCheckpointID;
+    public Vector3 lastCheckpointPosition;
+    public string lastCheckpointScene;
+    public void SetCurrentCheckpoint(CheckPointEnum id, Vector3 position, string sceneName)
+    {
+        lastCheckpointID = id;
+        lastCheckpointPosition = position;
+        lastCheckpointScene = sceneName;
+    }
+
+    public void RespawnPlayer(GameObject player)
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (lastCheckpointScene != currentScene)
+        {
+            // ⚠️ Scene khác -> load lại scene lưu checkpoint
+            SceneManager.LoadSceneAsync(lastCheckpointScene).completed += (op) =>
+            {
+                player.transform.position = lastCheckpointPosition;
+                FinishRespawn(player);
+            };
+        }
+        else
+        {
+            player.transform.position = lastCheckpointPosition;
+            FinishRespawn(player);
+        }
+    }
+
+    private void FinishRespawn(GameObject player)
+    {
+        CharacterController2D controller = player.GetComponent<CharacterController2D>();
+        if (controller != null)
+        {
+            controller.RestoreFullLife();
+        }
+    }
+}
