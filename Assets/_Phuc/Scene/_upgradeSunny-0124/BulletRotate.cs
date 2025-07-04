6bc6b36e5211dbf127cfc3bf89ab1f6961350ce1
trongphuc <phucnhtps39905@gmail.com>
2025-07-04 21:43:05 +0700

    upgrade/Sunny-0124 V1

---
+++ Assets/_Phuc/Scene/_upgradeSunny-0124/BulletRotate.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BulletRotate : MonoBehaviour
+{
+    public float lifetime = 4f;
+    public GameObject hitEffectGround;
+    public GameObject hitEffectEnemy;
+
+    [Header("Âm thanh va chạm")]
+    public AudioClip hitSoundGround;
+    public AudioClip hitSoundEnemy;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        RotateToVelocity();
+    }
+
+    void RotateToVelocity()
+    {
+        if (rb == null) return;
+
+        Vector2 dir = rb.linearVelocity;
+
+        if (dir.sqrMagnitude > 0.01f)
+        {
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            if (hitEffectGround != null)
+                Instantiate(hitEffectGround, transform.position, Quaternion.identity);
+
+            if (hitSoundGround != null)
+                AudioSource.PlayClipAtPoint(hitSoundGround, transform.position);
+
+            Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("NPC"))
+        {
+            if (hitEffectEnemy != null)
+                Instantiate(hitEffectEnemy, transform.position, Quaternion.identity);
+
+            if (hitSoundEnemy != null)
+                AudioSource.PlayClipAtPoint(hitSoundEnemy, transform.position);
+
+            Destroy(gameObject);
+        }
+    }
+}
