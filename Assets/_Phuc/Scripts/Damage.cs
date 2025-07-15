ba4356c1a226862a55e2084df31d611b9407b635
trongphuc <phucnhtps39905@gmail.com>
2025-07-15 20:15:29 +0700

    Fix Bug

---
+++ Assets/_Phuc/Scripts/Damage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public float damage = 1f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHealth pHealth = other.GetComponent<playerHealth>();
+            if (pHealth != null)
+            {
+                pHealth.TakeDamage(damage);
+            }
+        }
+    }
+
+    /*private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHealth pHealth = other.GetComponent<playerHealth>();
+            if (pHealth != null)
+            {
+                pHealth.TakeDamage(damage);
+            }
+        }
+    }*/
+}
