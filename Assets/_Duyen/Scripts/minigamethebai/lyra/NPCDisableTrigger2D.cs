fda6256d4becc9106a376326229eb2c024a6ede7
beduyencute <ngothuymyduyen1411@gmail.com>
2025-07-25 12:55:45 +0700

    bug/Sunny-0170_6

---
+++ Assets/_Duyen/Scripts/minigamethebai/lyra/NPCDisableTrigger2D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCDisableTrigger2D : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kiểm tra xem có FloatingFollower không
+        FloatingFollower follower = other.GetComponent<FloatingFollower>();
+        if (follower != null)
+        {
+            // Tắt NavMeshAgent nếu có (NavMeshAgent vẫn là 3D)
+            NavMeshAgent agent = other.GetComponent<NavMeshAgent>();
+            if (agent != null)
+            {
+                agent.isStopped = true;
+                agent.ResetPath();
+                agent.enabled = false;
+            }
+
+            // Tắt script FloatingFollower
+            follower.enabled = false;
+
+            // (Tùy chọn) Tắt Animator nếu cần
+            Animator anim = other.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.enabled = false;
+            }
+
+            Debug.Log("Đã vô hiệu hóa FloatingFollower + NavMeshAgent.");
+        }
+    }
+}
