0541567177462290f064d8ddd0e4c92424ebe1d8
nhtrongphuc <phucnhtps39905@gmail.com>
2025-08-21 22:50:41 +0700

    QuestSystem

---
+++ Assets/_Phuc/QuestSystem/scritps/MapQuestAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quests/Map Quest", fileName = "MapQuest_XXXX")]
+public class MapQuestAsset : ScriptableObject
+{
+    public string mapId; // Ví dụ: "MapCave", "Map1-1", ...
+    public Step[] steps;
+
+    [System.Serializable]
+    public class Step
+    {
+        [TextArea] public string text;
+        public bool showOnStart = false;      // Hiện ngay khi vào map/đến bước này
+        public string eventName;              // Tên sự kiện cần để sang bước này (để trống nếu chỉ là showOnStart)
+        public int requiredCount = 1;         // Dùng cho các bước cần số lượng (vd: thu thập 3 mã, diệt 10 địch)
+    }
+}
